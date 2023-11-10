c51a9583be2ea8dc2489e009a70b6f24b634033a
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2023-11-10 10:14:45 +0700

    learn controller n view

--- app/Controllers/Pages.php
+++ app/Controllers/Pages.php
@@ -6,6 +6,21 @@ class Pages extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $data = [
+            'title' => "Home | CI4LEARN",
+            'tes' => ["satu", "dua", "tiga"]
+        ];
+        echo view('layout/header', $data);
+        echo view('pages/home');
+        echo view('layout/footer');
+    }
+    public function about()
+    {
+        $data = [
+            'title' => "About | CI4LEARN"
+        ];
+        echo view('layout/header', $data);
+        echo view('pages/about');
+        echo view('layout/footer');
     }
 }
---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ducimus eum, quasi nisi reiciendis qui, quam blanditiis praesentium enim consequatur nesciunt nam odio molestias repudiandae voluptates dolorem dolores. Minima, itaque.</p>
+        </div>
+    </div>
+</div>
